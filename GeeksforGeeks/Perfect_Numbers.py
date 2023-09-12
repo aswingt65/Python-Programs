a1e7f29eb6ffeeb4b30b97ae6bf0c0c6cc3c8aab
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-09-12 23:14:35 +0530

    Create Perfect_Numbers.py

---
+++ GeeksforGeeks/Perfect_Numbers.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPerfectNumber(self, N):
+        # code here 
+        i, sum = 2, 1
+        while i**2 <= N:
+            if N % i == 0:
+               sum += (i + (N // i))
+            i += 1
+        if N == (i - 1)**2:
+           sum -= (i - 1)
+        return 1 if sum == N else 0
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        
+        ob = Solution()
+        print(ob.isPerfectNumber(N))
+# } Driver Code Ends
