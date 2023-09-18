d38ccb71bd5b43c1436acd7720ccadc47732055b
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-09-18 17:21:33 +0530

    Create Power_of_2.py

---
+++ GeeksforGeeks/Power_of_2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isPowerofTwo(self,n):
+        if n==0:
+            return 0
+        else:
+            return ((n & -n)^n == 0)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        if ob.isPowerofTwo(n):
+            print("YES")
+        else:
+            print("NO")
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
