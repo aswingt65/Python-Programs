c6a2c646de72f5f551c9269823f5b5ba36be180f
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-10-14 22:40:18 +0530

    Create Find_Common_Nodes_in_two_BSTs.py

---
+++ GeeksforGeeks/Find_Common_Nodes_in_two_BSTs.py
@@ -0,0 +1,104 @@
+class Solution:
+    
+    def findCommon(self, root1, root2):
+        hmap = {}
+        ls = []
+        self.inorder(root1, hmap, ls, False)
+        self.inorder(root2, hmap, ls, True)
+        return ls
+        
+    def inorder(self, root, hmap, ls, flag):
+        if root is None:
+            return 
+        
+        self.inorder(root.left, hmap, ls, flag)
+        if not flag:
+            hmap[root.data] = hmap.get(root.data, 0) + 1
+            
+        if flag and root.data in hmap:
+            ls.append(root.data)
+            
+        self.inorder(root.right, hmap, ls, flag)
+
+
+#{ 
+ # Driver Code Starts
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root1=buildTree(s)
+        s=input()
+        root2=buildTree(s)
+        ob = Solution()
+        res = ob.findCommon(root1, root2)
+        for i in range (len (res)):
+            print (res[i], end = " ")
+        print()
+# } Driver Code Ends
