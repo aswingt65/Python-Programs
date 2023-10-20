cdd4448c07ec90f492564e05be0bda3daef4f31f
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-10-20 18:42:27 +0530

    Create Form_a_number_divisible_by_3_using_array_digits.py

---
+++ GeeksforGeeks/Form_a_number_divisible_by_3_using_array_digits.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPossible(self, N, arr):
+        
+        if sum(arr) % 3 == 0:
+            return 1
+        return 0
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        arr = input().split()
+        for i in range(N):
+            arr[i] = int(arr[i])
+        
+        ob = Solution()
+        print(ob.isPossible(N, arr))
+# } Driver Code Ends
