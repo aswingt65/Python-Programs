d79ad9051190b731c1ef082c6e2767b570ecd05f
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-10-22 21:30:19 +0530

    Create Number_of_paths.py

---
+++ GeeksforGeeks/Number_of_paths.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberOfPaths (self, M, n):
+        path = 1
+        MOD = 10**9 + 7
+        for i in range(M - 1):
+            path = (path * (i + n)) % MOD
+            temp = pow(i + 1, MOD - 2, MOD)
+            path = (path * temp ) % MOD
+        return path
+
+#{ 
+ # Driver Code Starts
+
+if __name__ == '__main__': 
+    ob = Solution()
+    t = int (input ())
+    for _ in range (t):
+        M, N = map(int, input().split())
+        ans = ob.numberOfPaths(M, N)
+        print(ans)
+
+# } Driver Code Ends
